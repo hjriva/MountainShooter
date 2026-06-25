f2f26219a7addc3991c234eb534fee9fc59dad91
Helena Jung Riva <helenajugriva@gmail.com>
2026-06-25 17:05:35 -0300

    creating main menu

--- code/game.py
+++ code/game.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame as pg
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
         self.window = None
+        pg.init()
+        window = pg.display.set_mode(size=(600, 480))
 
     def run(self, ):
-        pass
+        print('Setup Start')
+
+        print('Setup Finish')
+
+        print('Loop Start')
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+            # Check for all events
+           # for event in pg.event.get():
+                #if event.type == pg.QUIT:
+                    #pg.quit()  # Close Window
+                        #quit()  # end pygame
+
